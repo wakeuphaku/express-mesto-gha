ac6a443fc4bdd6844639475d1a20adb4f755b12c
Dmitry Uchkin <not3hunnit@icloud.com>
2023-10-26 23:31:17 +0300

    Done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 const app = express();
 
 app.use(express.json());
+
 app.use((req, res, next) => {
   req.user = {
     _id: '6531c3b35d030560fdce9fef', // вставьте сюда _id созданного в предыдущем пункте пользователя
