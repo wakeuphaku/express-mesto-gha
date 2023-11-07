958cae661e83acd0c767aecce73ecda91261c990
Dmitry Uchkin <not3hunnit@icloud.com>
2023-11-08 00:53:33 +0300

    Done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { celebrate, Joi } = require('celebrate');
+
 const { PORT = 3000 } = process.env;
 
 const mongoose = require('mongoose');
@@ -10,10 +12,7 @@ const { createUsers, login } = require('./controllers/users');
 
 const auth = require('./middlewares/auth');
 
-const {
-  validationCreateUser,
-  validationLogin,
-} = require('./middlewares/validations');
+const regex = /^https?:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/im;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -23,8 +22,25 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/signin', validationLogin, login);
-app.post('/signup', validationCreateUser, createUsers);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(regex),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUsers);
 
 app.use(auth);
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,19 @@
 const router = require('express')
   .Router();
 const {
-  validationCreateUser,
-  validationPatchUser,
-  validationPatchAvatar,
-  validationGetUserId,
-} = require('../middlewares/validations');
+  celebrate,
+  Joi,
+} = require('celebrate');
+const isUrl = require('validator/lib/isURL');
+const BadRequest = require('../errors/BadRequest');
+
+const validationUrl = (url) => {
+  const validate = isUrl(url);
+  if (validate) {
+    return url;
+  }
+  throw new BadRequest('Некорректный URL');
+};
 const {
   getUsers,
   createUsers,
@@ -16,10 +24,35 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.post('/', validationCreateUser, createUsers);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validationUrl),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUsers);
 router.get('/me', getCurrentUser);
-router.get('/:userId', validationGetUserId, getUserId);
-router.patch('/me', validationPatchUser, patchUsers);
-router.patch('/me/avatar', validationPatchAvatar, patchAvatar);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().hex().length(24),
+  }),
+}), getUserId);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), patchUsers);
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().required().custom(validationUrl),
+    }),
+  }),
+  patchAvatar,
+);
 
 module.exports = router;
